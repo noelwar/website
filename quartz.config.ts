daf9df3ef99f83d08547649be92d971f8512463a
Christian NW <christian.warjri99@gmail.com>
2024-08-31 12:58:45 +0530

    Quartz sync: Aug 31, 2024, 12:58 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🕊 Christian's Digital Garden 🌺",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
